6a0bf147586447ee15414a593ecbe8495352996e
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2024-03-15 05:57:21 +0700

    fix(rootlayout): add meta google search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,23 @@ import Header from "@/components/header/Header";
 import { ContainerPage } from "./components/ContainerPage";
 import "@/styles/scroll.css";
 import { cookies } from "next/headers";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shop HCI - High-Quality Phones and Laptops Suitable for All Needs",
+  description:
+    "Welcome to HCI Shop - specializing in providing high-quality phones and laptops. Let us help you find the most suitable laptop and phone for your work and entertainment needs from our diverse collection of laptop",
+  openGraph: {
+    title: "HCI Shop",
+    description:
+      "Welcome to HCI Shop - specializing in providing high-quality phones and laptops. Let us help you find the most suitable laptop and phone for your work and entertainment needs from our diverse collection of laptop",
+    type: "website",
+    url: "https://hci-shop.vercel.app/",
+    images: [
+      "https://i.pinimg.com/564x/d7/47/d1/d747d1ca901acf7ed3561ffcd79a0720.jpg",
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,6 +33,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="fFtbMIgnPWJqb16Lu3O8cikrx6-Okm1cJDETfZ_vdeM"
+        />
+      </head>
       <body>
         <AppRouterCacheProvider>
           <ThemeRegistry modeInitial={(theme?.value as string) || "dark"}>
